efb0d5de0ec411c5e50ca46bbec90be1fe3538d5
isayme <isaymeorg@gmail.com>
2023-11-16 06:41:54 -0600

    Create hot.ts

---
+++ src/zhihu/hot.ts
@@ -0,0 +1,43 @@
+/**
+ * 知乎热榜
+ * see https://juejin.cn/post/7198434169982255164
+ */
+
+import lodash from 'lodash'
+import { getBrowser } from '../util/browser'
+import { formatYYYYMMDDHHmm } from '../util/date'
+import { dingtalkRobot } from '../util/dingtalk'
+import { runMain } from '../util/run'
+
+async function main() {
+  const browser = await getBrowser()
+
+  const page = await browser.newPage()
+
+  // 等待结果
+  await Promise.all([
+    page.goto('https://www.zhihu.com/billboard'),
+    page.waitForSelector('#js-initialData', { state: 'attached' }),
+  ])
+
+  let initialData = await page.locator('#js-initialData').innerText()
+  let { initialState } = JSON.parse(initialData)
+  let { topstory } = initialState
+  let { hotList } = topstory
+  hotList = hotList.slice(0, 20) // 保留20个
+
+  const title = `知乎热榜 ${formatYYYYMMDDHHmm(new Date())}`
+
+  let hotText = [title]
+
+  for (let item of hotList) {
+    let title = lodash.get(item, 'target.titleArea.text')
+    let url = lodash.get(item, 'target.link.url')
+    hotText.push(`- [${title}](${url})`)
+  }
+
+  await dingtalkRobot.markdown(title, hotText.join('\n'))
+
+  await browser.close()
+}
+runMain(main)
