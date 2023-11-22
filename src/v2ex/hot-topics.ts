e54d6c7a20772c106fb56d7b980fc260642bf011
isayme <isaymeorg@gmail.com>
2023-11-22 23:48:48 +0800

    优先用api

--- src/v2ex/hot-topics.ts
+++ src/v2ex/hot-topics.ts
@@ -1,25 +1,64 @@
+import axios from 'axios'
+import lodash from 'lodash'
 import { LaunchOptions } from 'playwright'
 import { firefox } from 'playwright-extra'
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+import { SocksProxyAgent } from 'socks-proxy-agent'
 import { formatYYYYMMDD } from '../util/date'
 import { dingtalkRobot } from '../util/dingtalk'
 import logger from '../util/logger'
 import { runMain } from '../util/run'
 
-async function main() {
+interface ITopic {
+  title: string
+  url: string
+}
+
+async function getHotTopicWithAPI(): Promise<ITopic[]> {
+  // https://github.com/djyde/V2EX-API
+
+  let httpAgent
+  let socks5Server = process.env.SOCKS5_SERVER
+  if (socks5Server) {
+    logger.info('axios 使用s5代理')
+
+    httpAgent = new SocksProxyAgent(socks5Server)
+  }
+
+  let result: ITopic[] = await axios
+    .request({
+      method: 'GET',
+      url: 'https://v2ex.com/api/topics/hot.json',
+      httpAgent,
+      httpsAgent: httpAgent,
+    })
+    .then((resp) => {
+      let { status, data } = resp
+      if (status >= 300) {
+        throw new Error(
+          `request fail status ${status}, respBody: ${JSON.stringify(data)}`,
+        )
+      }
+
+      return data
+    })
+
+  return result
+}
+
+async function getHotTopicWithPlaywright(): Promise<ITopic[]> {
   firefox.use(StealthPlugin())
 
   let opts: LaunchOptions = {}
 
   let socks5Server = process.env.SOCKS5_SERVER
   if (socks5Server) {
-    logger.info('使用s5代理')
+    logger.info('playwright 使用s5代理')
 
     opts.proxy = {
       server: socks5Server,
     }
   }
-  // const browser = await firefox.launch(opts)
   const browser = await firefox.launchPersistentContext('./.pw-cache', opts)
 
   const page = await browser.newPage()
@@ -37,21 +76,46 @@ async function main() {
     .locator('#TopicsHot .item_hot_topic_title a')
     .all()
 
+  let hotTopics = await Promise.all(
+    topicsHot.map(async function (topic) {
+      let [title, url] = await Promise.all([
+        topic.innerText(),
+        topic.getAttribute('href'),
+      ])
+
+      return { title, url }
+    }),
+  )
+
+  await browser.close()
+
+  return hotTopics
+}
+
+async function main() {
+  let hotTopics: ITopic[]
+  try {
+    hotTopics = await getHotTopicWithAPI()
+  } catch (e) {
+    logger.warn(`API获取失败 ${e}，改用 playwright`)
+    hotTopics = await getHotTopicWithPlaywright()
+  }
+
+  if (lodash.isEmpty(hotTopics)) {
+    logger.warn('无热议主题')
+    return
+  }
+
   const title = `V2EX ${formatYYYYMMDD(new Date())} 热议主题`
 
   let topicText = [title]
 
-  for (let topic of topicsHot) {
-    let [title, href] = await Promise.all([
-      topic.innerText(),
-      topic.getAttribute('href'),
-    ])
-    topicText.push(`- [${title}](https://v2ex.com${href})`)
+  for (let topic of hotTopics) {
+    let { title, url } = topic
+    topicText.push(`- [${title}](https://v2ex.com${url})`)
   }
 
   await dingtalkRobot.markdown(title, topicText.join('\n'))
-
-  await browser.close()
 }
 
 runMain(main)
